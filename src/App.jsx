fd08696e75428733100ebc949bc134405747f750
fpovina2004 <57961919+fpovina2004@users.noreply.github.com>
2021-02-28 01:40:59 -0300

    Ready for development :gear:

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap';
+import './sass/main.scss';
+
+
+const App = () => {
+    return (
+        <button className="btn btn-primary">
+            <h1>Hello world</h1>
+        </button>
+    );
+};
+
+export default App;
